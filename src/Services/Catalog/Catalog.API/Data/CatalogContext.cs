c968df9411280966b7c0c2dfd12cc63b2b4486c0
Alexandru Dragan <alexandru.dragan@gmail.com>
2021-11-20 10:03:20 +0200

    Basket API start

---
+++ src/Services/Catalog/Catalog.API/Data/CatalogContext.cs
@@ -0,0 +1,25 @@
+﻿using Catalog.API.Entities;
+using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
+
+namespace Catalog.API.Data
+{
+    public class CatalogContext : ICatalogContext
+    {
+        public CatalogContext(IConfiguration configuration)
+        {
+            var connectionStr = configuration.GetValue<string>("DatabaseSettings:ConnectionString");
+            var client = new MongoClient(connectionStr);
+
+            string dbName = configuration.GetValue<string>("DatabaseSettings:DatabaseName");
+            var database = client.GetDatabase(dbName);
+
+            string collectionName = configuration.GetValue<string>("DatabaseSettings:CollectionName");
+            Products = database.GetCollection<Product>(collectionName);
+
+            CatalogContextSeed.SeedData(Products);
+        }
+
+        public IMongoCollection<Product> Products { get; }
+    }
+}
