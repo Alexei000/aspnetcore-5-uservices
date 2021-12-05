e834588d227a13019d52baad19e2ecd7c7be5e74
Alexandru Dragan <alexandru.dragan@gmail.com>
2021-12-05 22:08:37 +0200

    added Order API

--- src/Services/Basket/Basket.API/Controllers/BasketController.cs
+++ src/Services/Basket/Basket.API/Controllers/BasketController.cs
@@ -1,4 +1,6 @@
-﻿using Basket.API.Entities;
+﻿using AutoMapper;
+using Basket.API.Entities;
+using Basket.API.GrpcServices;
 using Basket.API.Repositories;
 using Microsoft.AspNetCore.Mvc;
 using System;
@@ -13,14 +15,14 @@ namespace Basket.API.Controllers
     public class BasketController : ControllerBase
     {
         private readonly IBasketRepository _repository;
-        //private readonly DiscountGrpcService _discountGrpcService;
+        private readonly DiscountGrpcService _discountGrpcService;
         //private readonly IPublishEndpoint _publishEndpoint;
         //private readonly IMapper _mapper;
 
-        public BasketController(IBasketRepository repository /*, DiscountGrpcService discountGrpcService, IPublishEndpoint publishEndpoint, IMapper mapper */)
+        public BasketController(IBasketRepository repository, DiscountGrpcService discountGrpcService /*IPublishEndpoint publishEndpoint,*/ /*IMapper mapper*/)
         {
             _repository = repository ?? throw new ArgumentNullException(nameof(repository));
-            //_discountGrpcService = discountGrpcService ?? throw new ArgumentNullException(nameof(discountGrpcService));
+            _discountGrpcService = discountGrpcService ?? throw new ArgumentNullException(nameof(discountGrpcService));
             //_publishEndpoint = publishEndpoint ?? throw new ArgumentNullException(nameof(publishEndpoint));
             //_mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
         }
@@ -33,29 +35,30 @@ namespace Basket.API.Controllers
             return Ok(basket ?? new ShoppingCart(userName));
         }
 
-        [HttpPost]
-        [ProducesResponseType(typeof(ShoppingCart), (int)HttpStatusCode.OK)]
-        public async Task<ActionResult<ShoppingCart>> UpdateBasket([FromBody] ShoppingCart basket)
-        {
-            var ret = await _repository.UpdateBasket(basket);
-            return Ok(ret);
-        }
-
+        // legacy version
         //[HttpPost]
         //[ProducesResponseType(typeof(ShoppingCart), (int)HttpStatusCode.OK)]
         //public async Task<ActionResult<ShoppingCart>> UpdateBasket([FromBody] ShoppingCart basket)
         //{
-        //    // TODO : Communicate with Discount.Grpc
-        //    // and Calculate latest prices of product into shopping cart
-        //    // consume Discount Grpc
-        //    foreach (var item in basket.Items)
-        //    {
-        //        var coupon = await _discountGrpcService.GetDiscount(item.ProductName);
-        //        item.Price -= coupon.Amount;
-        //    }
-
-        //    return Ok(await _repository.UpdateBasket(basket));
+        //    var ret = await _repository.UpdateBasket(basket);
+        //    return Ok(ret);
         //}
+
+        [HttpPost]
+        [ProducesResponseType(typeof(ShoppingCart), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<ShoppingCart>> UpdateBasket([FromBody] ShoppingCart basket)
+        {
+            // TODO : Communicate with Discount.Grpc
+            // and Calculate latest prices of product into shopping cart
+            // consume Discount Grpc
+            foreach (var item in basket.Items)
+            {
+                var coupon = await _discountGrpcService.GetDiscount(item.ProductName);
+                item.Price -= coupon.Amount;
+            }
+
+            return Ok(await _repository.UpdateBasket(basket));
+        }
 
 
         [HttpDelete("{userName}", Name = "DeleteBasket")]
